0e243f8ec572cb289903e06aa875b9cf59341188
Praneet <praneetvkhp@gmail.com>
2025-07-04 17:39:47 +0530

    Auth

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
